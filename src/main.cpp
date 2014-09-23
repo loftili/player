16bd279a2305e61d9e9100f0ad0c76bc4c7914a4
Danny Hadley <dadley@sizethreestudios.com>
2014-09-22 21:43:46 -0400

    save

--- src/main.cpp
+++ src/main.cpp
@@ -1,155 +1,130 @@
 #include <iostream>
-#include <portaudio.h>
-#include <pthread.h>
+#include <sstream>
 #include <mpg123.h>
-#define FPB 4096
+#include <portaudio.h>
+#include <curl/curl.h>
+using namespace std;
+#define FRAME_PER_BUFFER 4096
 
 typedef PaStreamCallbackTimeInfo TimeInfo;
 typedef PaStreamCallbackFlags StreamFlags;
 typedef unsigned long FrameCount;
 
-typedef struct {
-  PaStream *stream;
-  int size;
-  float* buffer;
-  pthread_mutex_t mutex;
-  pthread_cond_t cond;
-  mpg123_handle* mh;
-} Portaudio;
-
-
-void* load_buffer(void *data) {
-  Portaudio* portaudio = (Portaudio*) data;
-  size_t done;
-
-  while(true) {
-    pthread_cond_wait(&portaudio->cond, &portaudio->mutex);
-    int err = mpg123_read(portaudio->mh, (unsigned char *) portaudio->buffer, portaudio->size * sizeof(float), &done);
-  }
-
-  pthread_exit(NULL);
-}
-
-int onPlay(const void* input, void* output, FrameCount fpb, const TimeInfo* time_info, StreamFlags flags, void* data) {
-  Portaudio* portaudio = (Portaudio*) data;
-  size_t done;
-  pthread_mutex_lock(&portaudio->mutex);
-
-  memcpy(output, portaudio->buffer, sizeof(float) * portaudio->size);
-
-  pthread_cond_broadcast(&portaudio->cond);
-  pthread_mutex_unlock(&portaudio->mutex);
-  return 0; 
-}
-
-int main(int argc, char* argv[]) {
-  std::string input;
-  if(argc < 2)
-    return 1;
-
-  std::string file_name = argv[1];
-  int success = Pa_Initialize();
-  int devices = Pa_GetDeviceCount();
-  int mpg_error = MPG123_OK;
-
-  if(success != paNoError || devices < 1) {
-    std::cout << "-!> portaudio initialize fail" << std::endl;
-    return 1;
-  }
-
-  std::cout << "-> Portaudio fine, moving onto mpg123" << std::endl;
-
-  int error = 0;
-  mpg123_init();
-  mpg123_handle* mh;
-  mh = mpg123_new(NULL, NULL);
-  
-  mpg_error = mpg123_open(mh, file_name.c_str());
-  if(mpg_error == MPG123_ERR) {
-    std::cout << "-!> mpg123 failed to open on a generic error file: " << mpg_error << std::endl;
-    mpg123_close(mh);
-    return 1;
-  } else if(mpg_error != MPG123_OK) {
-    std::cout << "-!> mpg123 failed to open the file: " << mpg_error << std::endl;
-    mpg123_close(mh);
-    return 1;
-  }
-  std::cout << "-> Mpg123 opened file fine, attempting to play" << std::endl;
-
-  mpg123_param(mh, MPG123_ADD_FLAGS, MPG123_FORCE_FLOAT, 0.);
-  
+struct AudioBuffer {
+  int length;
+  size_t realsize;
+  void* data;
+  mpg123_handle* mhandle;
+  int channels;
+  int encoding;
   long rate;
-  int channels, encoding;
-  mpg_error = mpg123_getformat(mh, &rate, &channels, &encoding);
-  if(mpg_error != MPG123_OK) {
-    std::cout << "-!> mpg123 failed to get format for the file: " << mpg_error << std::endl;
-    mpg123_close(mh);
-    return 1;
+};
+
+PaStream* pstream;
+AudioBuffer* audio_buffer;
+
+
+int playback(const void* in, void* out, FrameCount fpb, const TimeInfo* ti, StreamFlags f, void* d) {
+  AudioBuffer* ab = (AudioBuffer*) d;
+  size_t done;
+  off_t frame_offset;
+  unsigned char* audio;
+  int err = mpg123_decode_frame(ab->mhandle, &frame_offset, &audio, &done);
+  if(err != MPG123_OK) {
+    return -11;
   }
-
-  if(encoding != MPG123_ENC_FLOAT_32) {
-    std::cout << "-!> bad encoding " << std::endl;
-    mpg123_close(mh);
-    return 1;
-  }
-  mpg123_format_none(mh);
-  mpg123_format(mh, rate, channels, encoding);
-
-  std::cout << "-> Mpg123 got format information: ";
-  std::cout << "rate[" << rate << "] | ";
-  std::cout << "channels[" << channels << "] | ";
-  std::cout << "ecoding[" << encoding << "]" << std::endl;
-
-  int framesPerBuffer = FPB;
-  size_t done = 0;
-  Portaudio *portaudio = (Portaudio *) malloc(sizeof(Portaudio));
-  pthread_mutex_init(&portaudio->mutex, NULL);
-  pthread_cond_init(&portaudio->cond, NULL);
-  portaudio->size = framesPerBuffer * 2.0;
-  portaudio->buffer = (float *) malloc(sizeof(float) * portaudio->size);
-  portaudio->mh = mh;
-
-  int pa_error = Pa_OpenDefaultStream(&portaudio->stream, 0, 2, paFloat32, 44100, framesPerBuffer, onPlay, (void*)portaudio);
-
-  if(pa_error != paNoError) {
-    std::cout << "-!> failed opening port audio stream" << std::endl;
-    free(portaudio->buffer);
-    mpg123_close(mh);
-    mpg123_delete(mh);
-    return 1;
-  }
-  std::cout << "-> port audio opened a stream!" << std::endl;
-
-  int i;
-  pthread_t load_thread;
-  pthread_create(&load_thread, NULL, load_buffer, (void *)portaudio);
-
-  pa_error = Pa_StartStream(portaudio->stream);
-  if(pa_error != paNoError) {
-    std::cout << "-!> failed starting port audio stream" << std::endl;
-    free(portaudio->buffer);
-    mpg123_close(mh);
-    mpg123_delete(mh);
-    return 1;
-  }
-  std::cout << "-> port audio started a stream!" << std::endl;
-
-  while(input != "exit") {
-    std::cin >> input;
-  }
-
-  free(portaudio->buffer);
-  mpg123_close(mh);
-  mpg123_delete(mh);
-
-  Pa_StopStream(portaudio->stream);
-  Pa_CloseStream(portaudio->stream);
-
+  memcpy(out, audio, sizeof(float) * FRAME_PER_BUFFER * 2);
   return 0;
 }
 
-void print_message_function(void* data) {
-  std::cout << "whoa" << std::endl;
+size_t load_data(char* ptr, size_t size, size_t nmemb, void* userdata) {
+  AudioBuffer* buffer = (AudioBuffer*) userdata;
+  size_t realsize = size * nmemb;
+  buffer->data = realloc(buffer->data, buffer->length + realsize + 1);
+  void* end = &((char*)buffer->data)[buffer->length];
+  memcpy(end, ptr, realsize);
+  buffer->length += realsize;
+  ((char*)buffer->data)[buffer->length] = 0;
+  buffer->realsize += realsize;
+  return realsize;
 }
 
+void* fetch_and_play(void* url_buffer) {
+  cout << "initializing curl fetch for: " << (char*) url_buffer << endl;
+  CURL* curl_handle;
+  audio_buffer = (AudioBuffer*) malloc(sizeof(AudioBuffer));
+  audio_buffer->length = 0;
+  audio_buffer->realsize = 0;
+  audio_buffer->data = NULL;
 
+  curl_handle = curl_easy_init();
+  curl_easy_setopt(curl_handle, CURLOPT_URL, (char*) url_buffer);
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, load_data);
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, audio_buffer);
+  curl_easy_perform(curl_handle);
+  curl_easy_cleanup(curl_handle);
+
+  cout << "finished loading data... " << endl;
+  cout << "sending into mpg123" << endl;
+
+  mpg123_init();
+  mpg123_handle* mhandle = NULL;
+  mhandle = mpg123_new(NULL, NULL);
+  mpg123_open_feed(mhandle);
+  mpg123_feed(mhandle, (const unsigned char*)audio_buffer->data, audio_buffer->realsize);
+
+  unsigned char *audio;
+  size_t done;
+  off_t frame_offset;
+  int err = mpg123_decode_frame(mhandle, &frame_offset, &audio, &done);
+  if(err != MPG123_NEW_FORMAT) {
+    cout << "invalid mpg file, continuing" << endl;
+    return NULL;
+  }
+
+  audio_buffer->mhandle = mhandle;
+  cout << "seems to be a valid mpg file, continuing" << endl;
+  err = mpg123_getformat(mhandle, &audio_buffer->rate, &audio_buffer->channels, &audio_buffer->encoding);
+  if(err != MPG123_OK) {
+    cout << "totally invalid mpg file, continuing" << endl;
+    return NULL;
+  }
+  cout << "totally valid file, moving to portaudio" << endl;
+
+  err = Pa_Initialize();
+  int devices = Pa_GetDeviceCount();
+  if(err != paNoError || devices < 1) {
+    cout << "portaudio initialize fail" << endl;
+    return NULL;
+  }
+
+  err = Pa_OpenDefaultStream(&pstream, 0, 2, paFloat32, 44100, FRAME_PER_BUFFER, playback, (void*) &audio_buffer);
+  if(err != paNoError) {
+    cout << "portaudio open fail" << endl;
+    return NULL;
+  }
+  cout << "opened stream, starting!" << endl;
+  err = Pa_StartStream(pstream);
+  cout << "started stream, godspeed" << endl;
+}
+
+void play(string url) {
+  pthread_t play_thread;
+  pthread_create(&play_thread, NULL, fetch_and_play, (void*) url.c_str());
+}
+
+int main(int argc, char* argv[]) {
+  string url;
+  cout << "enter a url for us to play: " << endl;
+  cin >> url;
+  play(url);
+
+  cout << "type exit to close" << endl;
+  string exit;
+  do {
+    cin >> exit;
+  } while(exit != "exit");
+
+  free(audio_buffer);
+
+}
